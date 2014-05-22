ec53794e83e8e25ab5257d4019d2f33e2734d262
Matt Hippely <hippely@gmail.com>
2014-05-22 13:47:33 -0700

    SIteDNA: * Port vnCOnfig factory and add full test coverage.

--- app/scripts/services/vn-config.js
+++ app/scripts/services/vn-config.js
@@ -1,16 +1,216 @@
-'use strict';
+/**
+ * @ngdoc service
+ * @name vnApi
+ * @requires $q, $rootScope
+ * @description
+ *
+ * vConfig
+ * The vnConfig factory is used to manage user, account and app configuration that is more
+ * dynamic than can be easily accomplished with constnats or values. Well perhaps not values,
+ * Some of this should be depricated and moved into it's own value: currentAction, the tokens
+ * urls and perhaps all of the app state itself should live in a currentAppState value service.
+ * Hmmm. That's an interesting thought.
+ *
+ *
+ * ### vnApi exposes the following API
+ *
+ * - getAccount
+ * - getGlobalNavState
+ * - setGlobalNavState
+ * - getCurrentAction
+ * - setCurrentAction
+ * - getGlobalAttrBucketState
+ * - setGlobalAttrBucketState
+ * - getIframePathBase
+ * - initConfig
+ * - getFirebaseUrl
+ * - getScreenMode
+ * - setScreenMode
+ * - getPreviewMode
+ * - setPreviewMode
+ * - getWorkspaceDimensions
+ * - setWorkspaceDimensions
+ *
+ *
+ */
 
 angular.module('Volusion.toolboxCommon')
-    .factory('vnConfig', function () {
-        // Service logic
-        // ...
+    .factory('vnConfig', ['$q', '$rootScope', function ($q, $rootScope) {
 
-        var meaningOfLife = 42;
+        /**
+         * @function
+         * @name vnConfig
+         * @param $rootScope, $http
+         * @return an object with public api contract to get/set dynamic app configuation states.
+         */
+
+
+        'use strict';
+
+        var account,
+            apiToken,
+            apiUrl,
+            context,
+            currentAction = 'designAction',  // Start them here but if conf is persisted turn this into a function.
+            firebaseToken,
+            firebaseUrl,
+            globalAttrBucketState = true,    // Show the app attributes by default.
+            globalNavState = true,           // Show the app navigation by default.
+            iFramePathBase,
+            previewMode = false,             // Initial edit/preview mode
+            screenMode = 'desktop',          // Initial screen mode.
+            workspaceDimensions = {          // Initial height (use in Action section ... etc.)
+                width : 0,
+                height: 0
+            };
+
+        function getFirebaseUrlFn() {
+
+            /**
+             @Input: null
+             @Output: a string with the format https://YOURAPP.firebaseio.com
+             @Description: a string with the correct firebase url for accessing real time data set
+             */
+            return firebaseUrl;
+        }
+
+        function initConfigFn() {
+            /*
+             @function initConfigFn
+             @param - none
+             @return - none
+             @description - sets up the dynamic configuarion attributes for the app (iframe url base, firebase url, etc)
+             */
+
+            var mockResponse = {
+                api     : 'http://www.samplestore.io/api/v1',
+                account : 'asdf123',
+                context : 'SiteBuilder',
+                firebase: 'https://brilliant-fire-5600.firebaseio.com',
+                fbToken : ']idk - this comes from node server[',
+                apiToken: ']idk - how do I know if I am logging into edit[',
+                sandbox : 'http://localhost:8080'
+            };
+
+            //Simulate a admin login response
+            account = mockResponse.account;
+            apiToken = mockResponse.apiToken;
+            apiUrl = mockResponse.api;
+            context = mockResponse.context;
+            iFramePathBase = mockResponse.sandbox;
+            firebaseToken = mockResponse.fbToken;
+            firebaseUrl = mockResponse.firebase;
+
+            if (mockResponse && mockResponse.apiToken) {
+                $rootScope.$broadcast('vnSession.init', mockResponse);
+            }
+        }
+
+        function getAccountFn() {
+            /**
+             @function
+             @name getAccount
+             @description Return the account string for this account name
+             @param {}
+             @return String
+             */
+            return account;
+        }
+
+        function getIframePathBaseFn() {
+            if ('' === iFramePathBase) {
+                initConfigFn();
+            }
+            return iFramePathBase;
+        }
+
+        function getGlobalNavStateFn() {
+            return globalNavState;
+        }
+
+        function setGlobalNavStateFn(state) {
+            globalNavState = state;
+            $rootScope.$broadcast('vlnGlobalNavState.change', { state: state });
+        }
+
+        function setCurrentActionFn(action) {
+            /*
+             @Input a string
+             @Output broadcast a message (nothing returned)
+             @Purpose - update the string value of the currentAction for tha application
+
+             */
+            currentAction = action;
+            $rootScope.$broadcast('vlnCurrentAction.change', {action: action });
+        }
+
+        function getCurrentActionFn() {
+            /*
+             @Input - none
+             @Output - string value of currentAction
+             @Purpose - return the current value of the string currentAction
+             */
+            return currentAction;
+        }
+
+        function getGlobalAttrBucketStateFn() {
+            return globalAttrBucketState;
+        }
+
+        function setGlobalAttrBucketStateFn(state) {
+            globalAttrBucketState = state;
+            $rootScope.$broadcast('vlnGlobalAttrBucketState.change', { state: state });
+        }
+
+        function getScreenModeFn() {
+            return screenMode;
+        }
+
+        function setScreenModeFn(mode) {
+            screenMode = mode;
+        }
+
+        function getPreviewModeFn() {
+            return previewMode ? 'on' : 'off';
+        }
+
+        function setPreviewModeFn(mode) {
+            previewMode = mode;
+        }
+
+        function getWorkspaceDimensionsFn() {
+            return workspaceDimensions;
+        }
+
+        function setWorkspaceDimensionsFn(d) {
+
+            if(d && d.width && d.height) {
+                workspaceDimensions.height = d.height;
+                workspaceDimensions.width = d.width;
+                return;
+            }
+            throw new Error('Unable to set workspace dimensions.');
+
+
+        }
 
         // Public API here
         return {
-            someMethod: function () {
-                return meaningOfLife;
-            }
+            getAccount              : getAccountFn,
+            getGlobalNavState       : getGlobalNavStateFn,
+            setGlobalNavState       : setGlobalNavStateFn,
+            getCurrentAction        : getCurrentActionFn,
+            setCurrentAction        : setCurrentActionFn,
+            getGlobalAttrBucketState: getGlobalAttrBucketStateFn,
+            setGlobalAttrBucketState: setGlobalAttrBucketStateFn,
+            getIframePathBase       : getIframePathBaseFn,
+            initConfig              : initConfigFn,
+            getFirebaseUrl          : getFirebaseUrlFn,
+            getScreenMode           : getScreenModeFn,
+            setScreenMode           : setScreenModeFn,
+            getPreviewMode          : getPreviewModeFn,
+            setPreviewMode          : setPreviewModeFn,
+            getWorkspaceDimensions  : getWorkspaceDimensionsFn,
+            setWorkspaceDimensions  : setWorkspaceDimensionsFn
         };
-    });
+    }]);
--- test/spec/services/vn-config.js
+++ test/spec/services/vn-config.js
@@ -7,12 +7,161 @@ describe('Service: vnConfig', function () {
 
     // instantiate service
     var vnConfig;
-    beforeEach(inject(function (_vnConfig_) {
-        vnConfig = _vnConfig_;
-    }));
 
-    it('should do something', function () {
-        expect(!!vnConfig).toBe(true);
+    describe('before configuration', function () {
+        beforeEach(inject(function (_vnConfig_) {
+            vnConfig = _vnConfig_;
+        }));
+
+        it('should not have an account defined', function () {
+            expect(vnConfig.getAccount()).not.toBeDefined();
+        });
+
+        it('should default to true for globalNavState ', function () {
+            expect(vnConfig.getGlobalNavState()).toBe(true);
+        });
+
+        it('should default to the designAction ', function () {
+            expect(vnConfig.getCurrentAction()).toBe('designAction');
+        });
+
+        it('should default to true for the globalAttrBucketState', function () {
+            expect(vnConfig.getGlobalAttrBucketState()).toBe(true);
+        });
+
+        it('should not have an iframe base path', function () {
+            expect(vnConfig.getIframePathBase()).not.toBeDefined();
+        });
+
+        it('should not have a firebase endpoint', function () {
+            expect(vnConfig.getFirebaseUrl()).not.toBeDefined();
+        });
+
+        it('should default screenMode to desktop', function () {
+            expect(vnConfig.getScreenMode()).toBe('desktop');
+        });
+
+        it('should default to off for preview mode', function () {
+            expect(vnConfig.getPreviewMode()).toBe('off');
+        });
+
+        it('should have default workspace dimensions', function () {
+            var wsd = vnConfig.getWorkspaceDimensions();
+
+            expect(wsd).toBeDefined();
+            expect(wsd.width).toBeDefined();
+            expect(wsd.width).toBe(0);
+            expect(wsd.height).toBeDefined();
+            expect(wsd.height).toBe(0);
+        });
+
+    });
+
+    describe('after configuration', function () {
+        beforeEach(inject(function (_vnConfig_) {
+            vnConfig = _vnConfig_;
+
+            // TODO - Implicit test of initConfig fn needs to be made explicit
+            vnConfig.initConfig(); // The mock is baked into this method for dev
+        }));
+
+        it('should return an account', function () {
+            var testAccount = vnConfig.getAccount();
+            expect(testAccount).toBeDefined();
+            expect(typeof testAccount).toBe('string');
+        });
+
+        it('should return an globalNavState', function () {
+            var navState = vnConfig.getGlobalNavState();
+            expect(navState).toBeDefined();
+            expect(typeof navState).toBe('boolean');
+        });
+
+        it('should set a global nav state', function () {
+            vnConfig.setGlobalNavState(false);
+            expect(vnConfig.getGlobalNavState()).toBe(false);
+
+            vnConfig.setGlobalNavState(true);
+            expect(vnConfig.getGlobalNavState()).toBe(true);
+        });
+
+        it('should return a current action', function () {
+            var testAction = vnConfig.getCurrentAction();
+            expect(testAction).toBeDefined();
+            expect(typeof testAction).toBe('string');
+        });
+
+        it('should set a current action', function () {
+            //TODO - force only specific action types as string
+            var testAction = 'testAction';
+
+            vnConfig.setCurrentAction(testAction);
+            expect(vnConfig.getCurrentAction()).toBe('testAction');
+        });
+
+        it('should return an iFramePathBase', function () {
+            //TODO - answer why this doesn't have a setter. How is it set?
+            var test = vnConfig.getIframePathBase();
+
+            expect(test).toBeDefined();
+            expect(typeof test).toBe('string');
+        });
+
+        it('should return a firebase url ', function () {
+            var test = vnConfig.getFirebaseUrl();
+            expect(test).toBeDefined();
+            expect(typeof test).toBe('string');
+        });
+
+        it('should get the screenMode', function () {
+            var test = vnConfig.getScreenMode();
+            expect(test).toBeDefined();
+            expect(typeof test).toBe('string');
+        });
+
+        it('should set a screenMode', function () {
+            vnConfig.setScreenMode(true);
+            expect(vnConfig.getScreenMode()).toBe(true);
+
+            vnConfig.setScreenMode(false);
+            expect(vnConfig.getScreenMode()).toBe(false);
+        });
+
+
+        //TODO Talk w/ tsnako re the setter takes Boolean and the getter returns string
+        //TODO optimize it?
+        it('should get the preview mode', function () {
+            var test = vnConfig.getPreviewMode();
+            expect(test).toBeDefined();
+            expect(typeof test).toBe('string');
+        });
+
+        it('should set the preview mode', function () {
+            vnConfig.setPreviewMode(true);
+            expect(vnConfig.getPreviewMode()).toBe('on');
+
+            vnConfig.setPreviewMode(false);
+            expect(vnConfig.getPreviewMode()).toBe('off');
+        });
+
+        it('should get workspace dimensions', function () {
+            var test = vnConfig.getWorkspaceDimensions();
+            expect(test).toBeDefined();
+            expect(typeof test).toBe('object');
+        });
+
+        it('should set workspace dimensions', function () {
+            var mock = {
+                width: 11,
+                height: 99
+            };
+            vnConfig.setWorkspaceDimensions(mock);
+
+            var test = vnConfig.getWorkspaceDimensions();
+            expect(test.width).toEqual(11);
+            expect(test.height).toEqual(99);
+        });
+
     });
 
 });
