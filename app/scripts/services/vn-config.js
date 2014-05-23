e50b6408a44d2697f08a966128679ad0318db5c8
Matt Hippely <hippely@gmail.com>
2014-05-22 17:39:39 -0700

    SiteDNA: * Finish documentation for vnConfig * Finish adding tests for vnConfig

--- app/scripts/services/vn-config.js
+++ app/scripts/services/vn-config.js
@@ -42,12 +42,6 @@ angular.module('Volusion.toolboxCommon')
          * @returns {String} The string representing a Firebase resource
          */
         function getFirebaseUrl() {
-
-            /**
-             @Input: null
-             @Output: a string with the format https://YOURAPP.firebaseio.com
-             @Description: a string with the correct firebase url for accessing real time data set
-             */
             return firebaseUrl;
         }
 
@@ -58,19 +52,12 @@ angular.module('Volusion.toolboxCommon')
          * @methodOf Volusion.toolboxCommon.vnConfig
          * @description
          *
-         * #initConfig
          * Used in development as a mocking method to bootstrap with assumptions.
          * It sets up the dynamic configuration attributes for the app that prolly return from
          * and unknown api call at this point so we can use these properties: iframe url base,
          * firebase url, etc ...
          */
         function initConfig() {
-            /*
-             @function initConfig
-             @param - none
-             @return - none
-             @description -
-             */
 
             var mockResponse = {
                 api     : 'http://www.samplestore.io/api/v1',
@@ -146,60 +133,163 @@ angular.module('Volusion.toolboxCommon')
             return globalNavState;
         }
 
+        /**
+         * @ngdoc method
+         * @name setGlobalNavState
+         * @methodOf Volusion.toolboxCommon.vnConfig
+         * @param {Boolean} state - pass true to show the app navigation directive.
+         *
+         * @description
+         *
+         * Sets the globalNavState to the value passed in as a param. It $broadcasts a system
+         * wide message: vlnGlobalNavState.change with the param.
+         */
         function setGlobalNavState(state) {
             globalNavState = state;
             $rootScope.$broadcast('vlnGlobalNavState.change', { state: state });
         }
 
+        /**
+         * @ngdoc method
+         * @name setCurrentAction
+         * @methodOf Volusion.toolboxCommon.vnConfig
+         * @param {String} action - one of 'pageAction' or 'designAction'
+         *
+         * @description
+         *
+         * Set the app action to the param passed in and broadcast that change to the system.
+         */
         function setCurrentAction(action) {
-            /*
-             @Input a string
-             @Output broadcast a message (nothing returned)
-             @Purpose - update the string value of the currentAction for tha application
-
-             */
             currentAction = action;
             $rootScope.$broadcast('vlnCurrentAction.change', {action: action });
         }
 
+        /**
+         * @ngdoc method
+         * @name getCurrentAction
+         * @methodOf Volusion.toolboxCommon.vnConfig
+         * @return {String} currentAction
+         *
+         * @description
+         *
+         * Returns the vnConfig property value for currentAction
+         */
         function getCurrentAction() {
-            /*
-             @Input - none
-             @Output - string value of currentAction
-             @Purpose - return the current value of the string currentAction
-             */
             return currentAction;
         }
 
+        /**
+         * @ngdoc method
+         * @name getGlobalAttrBucketState
+         * @methodOf Volusion.toolboxCommon.vnConfig
+         * @return {Boolean} globalAttrBucketState
+         *
+         * @description
+         *
+         * Returns the vnConfig property vlaue for globalAttrBucketState
+         */
         function getGlobalAttrBucketState() {
             return globalAttrBucketState;
         }
 
+        /**
+         * @ngdoc method
+         * @name setGlobalAttrBucketState
+         * @methodOf Volusion.toolboxCommon.vnConfig
+         * @param {Boolean} state - true shows the AttrBucket Editor directive in SiteBuilder UI
+         *
+         * @description
+         *
+         * Sets the setGlobalAttrBucketState property for the vnCOnfig object to the value
+         * passed in as the state param. It broadcasts the vlnGlobalAttrBucketState.change
+         * notification.
+         */
         function setGlobalAttrBucketState(state) {
             globalAttrBucketState = state;
             $rootScope.$broadcast('vlnGlobalAttrBucketState.change', { state: state });
         }
 
+        /**
+         * @ngdoc method
+         * @name getScreenMode
+         * @methodOf Volusion.toolboxCommon.vnConfig
+         * @return {String} screenMode
+         *
+         * @description
+         *
+         * Return the vnConfig property value of screenMode.
+         */
         function getScreenMode() {
             return screenMode;
         }
 
+        /**
+         * @ngdoc method
+         * @name setScreenMode
+         * @methodOf Volusion.toolboxCommon.vnConfig
+         * @param {Boolean} mode - true enables preview mode / false enabled edit mode.
+         * @description
+         *
+         * Set the vnConfig property screenMode to the value of the mode param passed in.
+         */
         function setScreenMode(mode) {
             screenMode = mode;
         }
 
+        /**
+         * @ngdoc method
+         * @name getPreviewMode
+         * @methodOf Volusion.toolboxCommon.vnConfig
+         * @return {String} 'on' OR 'off' - is the output fromthe function.
+         *
+         * @description
+         *
+         * Use Boolean value of vnProperty previewMode to decide which string to return.
+         */
         function getPreviewMode() {
             return previewMode ? 'on' : 'off';
         }
 
+        /**
+         * @ngdoc method
+         * @name setPreviewMode
+         * @methodOf Volusion.toolboxCommon.vnConfig
+         * @param {String} mode - Can be one of 'desktop', '?tablet?' or '?phone?'
+         *
+         * @description
+         *
+         * Set the preview mode for the iframe by updating the vnConfig property to the value
+         * passed in with mode param.
+         */
         function setPreviewMode(mode) {
             previewMode = mode;
         }
 
+        /**
+         * @ngdoc method
+         * @name getWorkspaceDimensions
+         * @methodOf Volusion.toolboxCommon.vnConfig
+         * @return {String} workspaceDimensions
+         *
+         * @description
+         *
+         * Return the current value of the vnConfig property workspaceDimensions.
+         */
         function getWorkspaceDimensions() {
             return workspaceDimensions;
         }
 
+        /**
+         * @ngdoc method
+         * @name setWorkspaceDimensions
+         * @methodOf Volusion.toolboxCommon.vnConfig
+         * @param {Object} d - an object with two properties d.width & d.height
+         *
+         * @description
+         *
+         * Takes an object with width and height properties and sets the vnConfig
+         * workspaceDimension values for height and width.
+         */
         function setWorkspaceDimensions(d) {
 
             if(d && d.width && d.height) {
