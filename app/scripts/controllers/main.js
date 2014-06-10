49cf4cedec2c881dc44d2fb54f7b1af8d0faecd9
Tsanko Tsolov <tsanko_tsolov@volusion.com>
2014-06-10 11:11:59 -0700

    Updated libraries. Added unit tests and Jasmine custom matcher :) Some fixes - bug were found during writing of the tests.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,66 +3,66 @@
 angular.module('Volusion.toolboxCommon')
     .controller('MainCtrl',
 //        ['$scope', '$translate', '$translatePartialLoader',
-    ['$scope', '$translate', 'vnDataSrc',
-        function ($scope, $translate, vnDataSrc) {
-            //            function ($scope, $translate, $translatePartialLoader) {
+        ['$scope', '$translate', 'vnDataSrc',
+            function ($scope, $translate, vnDataSrc) {
+                //            function ($scope, $translate, $translatePartialLoader) {
 
-            'use strict';
+                'use strict';
 
-//            var params = {
-//                categoryId: null,
-//                filter    : 'featured',
-//                facets    : null,
-//                pageNumber: 1,
-//                pageSize  : 5
-//            };
-//
-//            var test = vnDataSrc.getProducts(params);
-            var test = vnDataSrc.getProducts();
-            console.log('MainCtrl test products query: ', test);
+    //            var params = {
+    //                categoryId: null,
+    //                filter    : 'featured',
+    //                facets    : null,
+    //                pageNumber: 1,
+    //                pageSize  : 5
+    //            };
+    //
+    //            var test = vnDataSrc.getProducts(params);
+                var test = vnDataSrc.getProducts();
+                console.log('MainCtrl test products query: ', test);
 
-            $scope.toggleLang = function () {
-                if ('en' === $translate.use()) {
-                    $translate.use('es');
-                } else {
-                    $translate.use($translate.preferredLanguage());
-                }
+                $scope.toggleLang = function () {
+                    if ('en' === $translate.use()) {
+                        $translate.use('es');
+                    } else {
+                        $translate.use($translate.preferredLanguage());
+                    }
 
-            };
+                };
 
-            // Mock category list
-            $scope.categoryList = [
-                {name            : 'Apparel',
-                    subCategories: [
-                        {id: 123, name: 'Women'},
-                        {id: 234, name: 'Men'}
-                    ]},
-                {name            : 'Home decor',
-                    subCategories: [
-                        {id: 123, name: 'Furniture'},
-                        {id: 234, name: 'Home Accessories'}
-                    ]},
-                {name            : 'Beauty',
-                    subCategories: [
-                        {id: 123, name: 'Bath and Body'},
-                        {id: 234, name: 'Hair Care'}
-                    ]},
-                {name            : 'Gourmet food',
-                    subCategories: [
-                        {id: 123, name: 'Speciality Items'},
-                        {id: 234, name: 'Sweets'}
-                    ]}
-            ];
+                // Mock category list
+                $scope.categoryList = [
+                    {name            : 'Apparel',
+                        subCategories: [
+                            {id: 123, name: 'Women'},
+                            {id: 234, name: 'Men'}
+                        ]},
+                    {name            : 'Home decor',
+                        subCategories: [
+                            {id: 123, name: 'Furniture'},
+                            {id: 234, name: 'Home Accessories'}
+                        ]},
+                    {name            : 'Beauty',
+                        subCategories: [
+                            {id: 123, name: 'Bath and Body'},
+                            {id: 234, name: 'Hair Care'}
+                        ]},
+                    {name            : 'Gourmet food',
+                        subCategories: [
+                            {id: 123, name: 'Speciality Items'},
+                            {id: 234, name: 'Sweets'}
+                        ]}
+                ];
 
-            // Mock image list
-            $scope.imageList = [
-                {src: 'http://lorempixel.com/450/300/people/0', alt: 'Random people image'},
-                {src: 'http://lorempixel.com/450/300/people/1', alt: 'Random people image'},
-                {src: 'http://lorempixel.com/450/300/people/2', alt: 'Random people image'},
-                {src: 'http://lorempixel.com/450/300/people/3', alt: 'Random people image'}
-            ];
+                // Mock image list
+                $scope.imageList = [
+                    {src: 'http://lorempixel.com/450/300/people/0', alt: 'Random people image'},
+                    {src: 'http://lorempixel.com/450/300/people/1', alt: 'Random people image'},
+                    {src: 'http://lorempixel.com/450/300/people/2', alt: 'Random people image'},
+                    {src: 'http://lorempixel.com/450/300/people/3', alt: 'Random people image'}
+                ];
 
-            // Mock rating
-            $scope.rating = 3;
+                // Mock rating
+                $scope.rating = '';
 
-        }]);
+            }]);
--- test/spec/directives/vnrating.js
+++ test/spec/directives/vnrating.js
@@ -1,8 +1,47 @@
-/*global angular, describe, module, beforeEach, inject, it, expect */
+/*global jasmine, angular, describe, module, beforeEach, inject, it, expect */
+
+var customMatchers = {
+    toBeRatedAs: function (util, customEqualityTesters) {
+
+        'use strict';
+
+        return {
+            compare: function (actual, expected) {
+                if (expected === undefined) {
+                    expected = '';
+                }
+
+                var idx,
+                    selected = 0,
+                    result = {};
+
+                for (idx = 0; idx < actual.length; idx++) {
+                    if (angular.element(actual[idx]).hasClass('filled')) {
+                        selected++;
+                    }
+                }
+
+                result.pass = util.equals(selected, expected, customEqualityTesters);
+
+                if (result.pass) {
+                    result.message = 'Rating is set to ' + expected + '.';
+                } else {
+                    result.message = 'Expected rating to be ' + expected + ' instead of ' + selected + '.';
+                }
+
+                return result;
+            }
+        };
+    }
+};
 
 describe('Directive: vnRating', function () {
 
     'use strict';
+
+    beforeEach(function () {
+        jasmine.addMatchers(customMatchers);
+    });
 
     // load the directive's module
     beforeEach(module('Volusion.toolboxCommon'));
@@ -10,13 +49,46 @@ describe('Directive: vnRating', function () {
     var element,
         scope;
 
-    beforeEach(inject(function ($rootScope) {
+    beforeEach(inject(function ($rootScope, $compile) {
         scope = $rootScope.$new();
+        scope.rating = 2;
+
+        element = angular.element('<vn-rating rating-value="rating"></vn-rating>');
+
+        $compile(element)(scope);
+        scope.$digest();
     }));
 
-    it('should make hidden element visible', inject(function ($compile) {
-        element = angular.element('<vn-rating></vn-rating>');
-        element = $compile(element)(scope);
-        expect(element.text()).toBe('');
-    }));
+    it('should have container element w/ specific class name', function () {
+        expect(element.attr('class')).toContain('vn-rating');
+    });
+
+    it('should have list', function () {
+        var list = element.find('ul');
+
+        expect(list.length).toBe(1);
+    });
+
+    it('should have 5 list items', function () {
+        var list = element.find('ul');
+
+        expect(list.eq(0).find('li').length).toEqual(5);
+    });
+
+    it('should have 2 list items with class "filled"', function () {
+        var list = element.find('ul'),
+            items = list.eq(0).find('li');
+
+        expect(items).toBeRatedAs(scope.rating);
+    });
+
+    it('should have 5 list items with class "filled"', function () {
+        scope.rating = 5;
+        scope.$digest();
+
+        var list = element.find('ul'),
+            items = list.eq(0).find('li');
+
+        expect(items).toBeRatedAs(scope.rating);
+    });
 });
