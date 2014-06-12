f62034a1dbe5ab83f45d511e15524c667577d9c6
Matt Hippely <hippely@gmail.com>
2014-06-12 16:24:38 -0700

    Refactoring Method: * Port common tools from method into the toolbox * Created vn-block & vn-element directives * Created bem-factory
    Refactoring:
    * Removed bem related directive/services for now. Not needed at thtis time.
    
    Refactoring
    * Ported vn-meta-tag directive
    * Added test code but commented out (mocha specific)
    
    Refactoring:
    * Adding files for vn-meta-tags

---
+++ app/scripts/directives/vn-meta-tags.js
@@ -0,0 +1,91 @@
+/*global angular */
+
+/**
+ * @ngdoc directive
+ * @name Volusion.toolboxCommon.directive:vnMetaTags
+ * @restrict EA
+ * @requires NotApplicableYet ported from Method Theme
+ * @description
+ *
+ * #vnMetaTags Directive
+ * Given the seo data from a product or category (Need-to-confirm), update these meta tags:
+ * - title
+ * - description
+ * - keywords
+ * - toAppend ?? Not sure what this is
+ * - robots
+ *
+ * Dev Note, these were pulled directly from the elements method theme was watching.
+ *
+ * @usage
+ <head data-vn-meta-tags data-title="seo.metaTagTitle"
+ data-description="seo.metaTagDescription" data-keywords="seo.metaTagKeywords"
+ data-robots="seo.enableRobotsMetatags" data-to-append="seo.globallyAppendedMetatags">
+
+ */
+angular.module('Volusion.toolboxCommon')
+    .directive('vnMetaTags', function () {
+        'use strict';
+
+        return {
+            restrict: 'EA',
+            scope   : {
+                title      : '=',
+                description: '=',
+                keywords   : '=',
+                toAppend   : '=',
+                robots     : '='
+            },
+            link    : function (scope, elem) {
+
+                var appendElement = function (elementToAppend) {
+                    if (typeof elementToAppend !== 'undefined') {
+                        elem.append(elementToAppend);
+                    }
+                };
+
+                var setTitleTag = function (titleText) {
+                    var titleTag = elem.find('title');
+                    if (titleTag.length > 0) {
+                        titleTag.remove();
+                    }
+                    if (titleText) {
+                        elem.append(angular.element('<title/>').text(titleText));
+                    }
+                };
+
+                var setMetaTag = function (metaTagName, metaTagContent) {
+                    var metaTag = elem.find('meta[name=' + metaTagName + ']');
+
+                    if (metaTag.length > 0) {
+                        metaTag.remove();
+                    }
+                    if (metaTagContent) {
+                        elem.append(angular.element('<meta/>').attr('name', metaTagName).
+                            attr('content', metaTagContent));
+                    }
+                };
+
+                var setDescription = function (description) {
+                    setMetaTag('description', description);
+                };
+
+                var setKeywords = function (keywords) {
+                    setMetaTag('keywords', keywords);
+                };
+
+                scope.$watch('title', setTitleTag);
+                scope.$watch('description', setDescription);
+                scope.$watch('keywords', setKeywords);
+                scope.$watch('toAppend', appendElement);
+                scope.$watch('robots', function (newValue) {
+                    if (typeof newValue !== 'undefined' &&
+                        JSON.parse(newValue) === true) {
+                        setMetaTag('robots', 'index,follow');
+                        setMetaTag('GOOGLEBOT', 'INDEX,FOLLOW');
+                    }
+                });
+            }
+        };
+        ;
+    });
