41fbd0a1d17e4a0b5589a045df419410554760a5
Ken Jackson <ken@kenjacksondesign.com>
2014-07-24 13:58:10 -0500

    Fixed bug in paramsObject to use page instead of pageNumber

--- dist/vn-toolbox-common.js
+++ dist/vn-toolbox-common.js
@@ -1,4 +1,4 @@
-/*! vn-toolbox-common - ver.0.0.8 (2014-07-23) */
+/*! vn-toolbox-common - ver.0.0.8 (2014-07-24) */
 angular.module('Volusion.toolboxCommon.templates', []);
 angular.module('Volusion.toolboxCommon', [
   'pascalprecht.translate',
@@ -1570,9 +1570,7 @@ angular.module('Volusion.toolboxCommon').factory('vnProductParams', function () 
 		 */
     facets = [],
     // Container for the facets to query for
-    //        currentPageNumber = '',
-    //        nextPageNumber = '',
-    //        previousPageNumber = '',
+    //        currentPage = '',
     /**
 		 * @ngdoc property
 		 * @name paramsObject
@@ -1591,9 +1589,31 @@ angular.module('Volusion.toolboxCommon').factory('vnProductParams', function () 
       maxPrice: '',
       accessoriesOf: '',
       sort: '',
-      pageNumber: '',
+      page: '',
       pageSize: ''
     };
+  /**
+		 * @ngdoc function
+		 * @name nextPage
+		 * @methodOf Volusion.toolboxCommon.vnProductParams
+		 *
+		 * @description
+		 * Sets the paramsObject page property to the next page.
+		*/
+  function nextPage() {
+    paramsObject.page++;
+  }
+  /**
+		 * @ngdoc function
+		 * @name previousPage
+		 * @methodOf Volusion.toolboxCommon.vnProductParams
+		 *
+		 * @description
+		 * Sets the paramsObject page property to the previous page.
+		*/
+  function previousPage() {
+    paramsObject.page--;
+  }
   /**
 		 * @ngdoc function
 		 * @name setSort
@@ -1848,7 +1868,7 @@ angular.module('Volusion.toolboxCommon').factory('vnProductParams', function () 
       maxPrice: '',
       accessoriesOf: '',
       sort: '',
-      pageNumber: '',
+      page: '',
       pageSize: ''
     };
   }
@@ -1909,46 +1929,46 @@ angular.module('Volusion.toolboxCommon').factory('vnProductParams', function () 
   }
   /**
 		 * @ngdoc function
-		 * @name setPageNumber
-		 * @param {String} setPageNumber is a string that can be passed to api to modify the page to ask backend for
+		 * @name setPage
+		 * @param {String} setPage is a string that can be passed to api to modify the page to ask backend for
 		 * @methodOf Volusion.toolboxCommon.vnProductParams
 		 *
 		 * @description
-		 * Setter for the paramsObject pageNumber property.
+		 * Setter for the paramsObject page property.
 		 */
-  function setPageNumber(pageNumber) {
-    paramsObject.pageNumber = pageNumber;
+  function setPage(page) {
+    paramsObject.page = page;
   }
   /**
 		 * @ngdoc function
-		 * @name getPageNumber
+		 * @name getPage
 		 * @methodOf Volusion.toolboxCommon.vnProductParams
 		 *
 		 * @description
-		 * Getter for the paramsObject pageNumber property.
+		 * Getter for the paramsObject page property.
 		 */
-  function getPageNumber() {
-    return paramsObject.pageNumber;
+  function getPage() {
+    return paramsObject.page;
   }
   /**
 		 * @ngdoc function
-		 * @name setPageNumber
-		 * @param {String} setPageNumber is a string that can be passed to api to modify the page to ask backend for
+		 * @name setPageSize
+		 * @param {String} setPageSize is a string that can be passed to api to modify the page to ask backend for
 		 * @methodOf Volusion.toolboxCommon.vnProductParams
 		 *
 		 * @description
-		 * Setter for the paramsObject pageNumber property.
+		 * Setter for the paramsObject page property.
 		 */
   function setPageSize(pageSize) {
     paramsObject.pageSize = pageSize;
   }
   /**
 		 * @ngdoc function
-		 * @name getPageNumber
+		 * @name getPageSize
 		 * @methodOf Volusion.toolboxCommon.vnProductParams
 		 *
 		 * @description
-		 * Getter for the paramsObject pageNumber property.
+		 * Getter for the paramsObject pageSize property.
 		 */
   function getPageSize() {
     return paramsObject.pageSize;
@@ -1961,11 +1981,13 @@ angular.module('Volusion.toolboxCommon').factory('vnProductParams', function () 
     getFacetString: getFacetString,
     getMinPrice: getMinPrice,
     getMaxPrice: getMaxPrice,
-    getPageNumber: getPageNumber,
+    getPage: getPage,
     getPageSize: getPageSize,
     getParamsObject: getParamsObject,
     getSort: getSort,
     isFacetSelected: isFacetSelected,
+    nextPage: nextPage,
+    previousPage: previousPage,
     removeSlug: removeSlug,
     removeSearch: removeSearch,
     setMinPrice: setMinPrice,
@@ -1978,7 +2000,7 @@ angular.module('Volusion.toolboxCommon').factory('vnProductParams', function () 
     resetParamsObject: resetParamsObject,
     setAccessoriesOf: setAccessoriesOf,
     setMaxPrice: setMaxPrice,
-    setPageNumber: setPageNumber,
+    setPage: setPage,
     setPageSize: setPageSize,
     setSort: setSort,
     updateSearch: updateSearch,
