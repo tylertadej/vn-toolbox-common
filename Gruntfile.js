f2df17990507e2d402e8572951a3f78077565733
Matt Hippely <hippely@gmail.com>
2014-07-25 16:07:24 -0700

    Small change to gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -274,7 +274,7 @@ module.exports = function(grunt) {
 			}
 		},
 
-		// ngmin tries to make the code safe for minification automatically by
+		// ngAnnotate tries to make the code safe for minification automatically by
 		// using the Angular long form for dependency injection. It doesn't work on
 		// things like resolve or inject so those have to be done manually.
 		ngAnnotate: {
