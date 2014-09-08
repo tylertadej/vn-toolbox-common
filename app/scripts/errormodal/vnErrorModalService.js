bd8f019db17f8b543ea66e40b6d2cefeb831c1d1
Prabin Varma <prabinv@gmail.com>
2014-09-08 10:26:42 -0500

    Added the vnErrorModalService to show the server error modal

---
+++ app/scripts/errormodal/vnErrorModalService.js
@@ -0,0 +1,23 @@
+'use strict';
+/**
+ * @ngdoc service
+ * @name Volusion.toolboxCommon.vnErrorModalService
+ * @description
+ * The vnErrorModalService provides methods to show a
+ * modal window with template passed in or the default
+ * error template.
+ *
+ * */
+
+
+angular.module('Volusion.toolboxCommon')
+    .factory('ErrorModalService', ['$modal', function($modal){
+
+    return {
+        showError: function(errorViewTemplateUrl) {
+            return $modal.open({
+                templateUrl: errorViewTemplateUrl || 'errormodal/vnErrorModal.tpl.html'
+            });
+        }
+    };
+}]);
