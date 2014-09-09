4622eeb302e3d60627061b80479378e5fefe704e
Prabin Varma <prabinv@gmail.com>
2014-09-09 11:18:57 -0500

    Moved cache bust filter and token gen svc

--- dist/vn-toolbox-common.js
+++ dist/vn-toolbox-common.js
@@ -137,6 +137,69 @@ angular.module('Volusion.toolboxCommon')
         return self;
     }]);
 
+'use strict';
+
+/**
+ * @ngdoc filter
+ * @name Volusion.toolboxCommon:vnCacheBust
+ *
+ * @description
+ * The `vnCacheBust` filter adds a cache busting token to the end of the
+ * URL string passed in.
+ *
+ * @requires
+ * vnTokenGenerator
+ *
+ * @usage
+ * <a ng-href="foo.someUrl | vnCacheBust">Bar</a>
+ *
+ * @example
+ */
+
+angular.module('Volusion.toolboxCommon').filter('vnCacheBust', [
+    'vnTokenGenerator',
+    function (vnTokenGenerator) {
+
+
+        function appendTokenToUrl(url) {
+            if (!url || !url.trim()) {
+                return url;
+            }
+
+            var separator = (url.indexOf('?') > -1) ? '&' : '?';
+            return url + separator + '_=' + vnTokenGenerator.getCacheBustingToken();
+        }
+
+        return function (url) {
+            return appendTokenToUrl(url);
+        };
+    }
+]);
+
+'use strict';
+/**
+ * @ngdoc service
+ * @name Volusion.toolboxCommon:vnTokenGenerator
+ *
+ * @description
+ * # vnTokenGenerator
+ * The vnTokenGenerator service is used to generate a unique
+ * token which can be used for cache busting
+ *
+ */
+
+angular.module('Volusion.toolboxCommon')
+	.factory('vnTokenGenerator', function () {
+
+		function getCacheBustingToken() {
+			return (new Date()).valueOf();
+		}
+
+		return {
+			getCacheBustingToken: getCacheBustingToken
+		};
+	});
+
 angular.module('Volusion.toolboxCommon')
 	.controller('OptionsCtrl', ['$rootScope','$scope',
 		function($rootScope, $scope) {
