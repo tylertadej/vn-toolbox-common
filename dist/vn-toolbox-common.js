85bbcc4a229394a69c1e6ceeae072f2630e58c55
Prabin Varma <prabinv@gmail.com>
2014-09-09 10:51:34 -0500

    moved scroll-to-anchor directive to toolbox

--- dist/vn-toolbox-common.js
+++ dist/vn-toolbox-common.js
@@ -1637,6 +1637,43 @@ angular.module('Volusion.toolboxCommon')
 			};
 		});
 
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name Volusion.toolboxCommon.directive:vnScrollToAnchor
+ * @restrict A
+ * @scope
+ *
+ *
+ * @description
+ * Directive to automatically scroll to the `hash` location in
+ * the page.
+ *
+ * @usage
+ *
+ * @example
+ */
+
+angular.module('Volusion.toolboxCommon')
+	.directive('vnScrollToAnchor', ['$location', '$anchorScroll',
+		function ($location, $anchorScroll) {
+
+			return {
+				restrict: 'AC',
+				compile : function () {
+
+					return function (scope, element, attr) {
+						element.bind('click', function (event) {
+							event.preventDefault();
+							$location.hash(attr.vnScrollToAnchor);
+							$anchorScroll();
+						});
+					};
+				}
+			};
+		}]);
+
 angular.module('Volusion.toolboxCommon')
     .value('vnApiConfigurations', {});
 
