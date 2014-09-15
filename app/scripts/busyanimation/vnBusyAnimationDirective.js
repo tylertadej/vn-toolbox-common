746dfadb4c99ff3a28dbe20f3842d7b05ba27e26
Tsanko Tsolov <tsanko_tsolov@volusion.com>
2014-09-15 16:50:59 -0700

    Separate html template from directive.

--- app/scripts/busyanimation/vnBusyAnimationDirective.js
+++ app/scripts/busyanimation/vnBusyAnimationDirective.js
@@ -25,7 +25,7 @@ angular.module('Volusion.toolboxCommon')
 		'use strict';
 
 		return {
-			templateUrl: 'template/busy-animation.html',
+			templateUrl: 'busyanimation/vnBusyAnimation.tpl.html',
 			transclude : true,
 			restrict: 'A',
 			link    : function postLink(scope, element, attrs) {
@@ -37,31 +37,4 @@ angular.module('Volusion.toolboxCommon')
 				});
 			}
 		};
-	})
-	.run(['$templateCache', function ($templateCache) {
-
-		'use strict';
-
-		$templateCache.put(
-			'template/busy-animation.html',
-			'<ng-transclude></ng-transclude>' +
-			'<div class="{{ class }}" title="1" data-ng-if="show">' +
-				'<svg version="1.1" id="loader-1" ' +
-					'xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" ' +
-					'x="0px" y="0px" width="40px" height="40px" ' +
-					'viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">' +
-
-					'<path fill="#000" d="M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z">' +
-						'<animateTransform ' +
-							'attributeType="xml" ' +
-							'attributeName="transform" ' +
-							'type="rotate" ' +
-							'from="0 25 25" ' +
-							'to="360 25 25" ' +
-							'dur=".6s" ' +
-							'repeatCount="indefinite"/>' +
-					'</path>' +
-				'</svg>' +
-			'</div>'
-		);
-	}]);
+	});
